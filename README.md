d47e5aceb6af40bf61cb9aea305c016274e68b59
Paul <pauljb1975@gmail.com>
2021-03-30 02:22:11 -0400

    Added validation check on the response, and a console output option to show every check

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Windows desktop notifier for Rite Aid COVID-19 vaccine appointment slots
 Store numbers are stored as strings and leading zeros aren't necessary
 ie. stores=['11144','11143','3768']
 
-When it finds availability, it will beep the number of times specified (default 100)
+When it finds availability, it will beep the number of times specified by numBeeps (default 100)
+
+Set enableOutput to False if you only want to see when a shot is found.
+Set enableOutput to True (default) to see the results of each request
 
--- notify.py
+++ notify.py
@@ -1,22 +1,33 @@
-import requests,json,time,winsound
+import requests,json,time,winsound,datetime
 
 while True:
   #add a list of store numbers near your zip code as 
-	stores=["<####>","<####>","<####>"]
+	stores=["###","####","####"]
   # number of times to beep if availability is found
 	numBeeps=100
+	enableOutput=True
 
 	for store in stores:
+		if enableOutput==True:
+			ct = datetime.datetime.now()
+			print(ct, "Checking store",store,". . . ",end='')
 		url = "https://www.riteaid.com/services/ext/v2/vaccine/checkSlots?storeNumber="+store
 		headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36', 'Content-Type':'application/json'}
 		result = requests.get(url, headers=headers)
 		output = json.loads(result.content.decode())
 
-		if output ['Data']['slots']['1'] == False:
-			time.sleep(3)
-			continue
+		if output ['Status'] == 'SUCCESS':
+			if output ['Data']['slots']['1'] == False:
+				if enableOutput==True:
+					print("No slots")
+				time.sleep(3)
+				continue
+			else:
+				print(store,' Has Shots!!!')
+				for x in range(0, numBeeps):
+					winsound.Beep(650, 250)
+				continue
 		else:
-			print(store,' Has Shots')
-			for x in range(0, numBeeps):
-				winsound.Beep(650, 250)
+			if enableOutput==True:
+				print("Error in response")
 			continue
